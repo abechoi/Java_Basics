7879415b2fd0ab70029062cb6f5c0fa3f5404c0f
abechoi <abraham.choi@icloud.com>
2019-08-22 19:41:58 -0700

    Loops - added foreach loop

--- loops.java
+++ loops.java
@@ -1,6 +1,7 @@
 /*
 	LOOPS:
 	1. while, do-while, and for loops
+	2. Added foreach loop
 */
 
 public class loops{
@@ -8,6 +9,7 @@ public class loops{
 	public static void main(String[] args){
 
 		int x = 0;
+		int[] arr = new int[10];
 
 		// WHILELOOP1
 		System.out.println( "\nWHILELOOP1" );
@@ -32,8 +34,24 @@ public class loops{
 		for( int i = 0; i < 10; ++i ){
 
 			System.out.printf( "i = %d\n", i );
+
+			arr[i] = i; // fill array for the foreach loop
 		}
 
+		// FOREACHLOOP1
+		System.out.println( "\nFOREACHLOOP1" );
+		for( int i : arr ){
+
+			System.out.printf( "i = %d\n", i );
+		}
 
 	}// END MAIN
 }// END CLASS
+
+
+
+
+
+
+
+
